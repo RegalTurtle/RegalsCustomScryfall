111bf0e6be082cc7fcaeec06e75d748814038af7
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-07-11 21:01:47 -0700

    Resetting to what it was before all of the recent changes

--- index.js
+++ index.js
@@ -8,32 +8,16 @@ var app = express();
 // Serve static files from the 'client' directory
 app.use(express.static(path.join(__dirname, 'client')));
 
-// Middleware to inject console log message into HTML
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'), function (err) {
-    if (err) {
-      res.status(err.status).end();
-    } else {
-      // Inject the script if HTML is successfully sent
-      res.write(`
-        <script>
-          console.log('This message is sent from the server.');
-        </script>
-      `);
-      res.end();
-    }
-  });
+// Use the following to handle JSON files specifically if needed
+app.get('/customcards.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/customcards.json'));
 });
 
-// // Use the following to handle JSON files specifically if needed
-// app.get('/customcards.json', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/customcards.json'));
-// });
+// Serve the main index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
-// // Serve the main index.html file
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/index.html'));
-// });
 
 // app.get('/*', (req, res) => { // If a client file is asked for, give it and specify the correct MIME type
 //   // Connect to the correct dir of the file hierarchy
