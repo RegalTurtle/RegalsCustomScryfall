4387a2c186cf7accecb43dba241ced3e6ce635dd
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-07-12 15:56:02 -0700

    Testing client folder

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('*/customcards.json', (req, res) => {
 });
 
 app.get('*/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, 'style.css'));
+  res.sendFile(path.join(__dirname, 'client/style.css'));
 });
 
 // // Serve the main index.html file
