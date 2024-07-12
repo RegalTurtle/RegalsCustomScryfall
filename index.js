14bae68f05ee91a0dc935f63ecfa78c20b00d567
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-07-11 18:58:02 -0700

    Trying without the client folder

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 // Use the following to handle JSON files specifically if needed
 app.get('/customcards.json', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/customcards.json'));
+  res.sendFile(path.join(__dirname, 'customcards.json'));
 });
 
 // Serve the main index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // app.get('/*', (req, res) => { // If a client file is asked for, give it and specify the correct MIME type
