74cc864fc15b584bc9fa0dd44532f3d968304189
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-07-11 21:52:11 -0700

    Random bullshit go

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.get('*/style.css', (req, res) => {
   res.sendFile(path.join(__dirname, 'style.css'));
 });
 
+// Use the following to handle JSON files specifically if needed
+app.get('/customcards.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'customcards.json'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'style.css'));
+});
+
 // Serve the main index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'home.html'));
