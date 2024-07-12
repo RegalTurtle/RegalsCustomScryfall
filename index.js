31d34eb29dd90aac7e004681aa33b531b60578c4
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-07-11 18:53:05 -0700

    Trying to not use path

--- index.js
+++ index.js
@@ -5,24 +5,17 @@ express.json("Access-Control-Allow-Origin", "*");
 var mime = require('mime-types');
 var app = express();
 
-//Idiomatic expression in express to route and respond to a client request
-// app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-//   res.sendFile('client/index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-//                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
-// });
-
 // Serve static files from the 'client' directory
-app.use(express.static(path.join(__dirname, 'client')));
-
+app.use(express.static(__dirname, 'client'));
 
 // Use the following to handle JSON files specifically if needed
 app.get('/customcards.json', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/customcards.json'));
+  res.sendFile(__dirname + '/client/customcards.json');
 });
 
 // Serve the main index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/index.html'));
+  res.sendFile(__dirname + '/client/index.html');
 });
 
 // app.get('/*', (req, res) => { // If a client file is asked for, give it and specify the correct MIME type
