43f33cdcfb75a37c7db7916ab8f19168f79a825a
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-07-13 22:24:55 -0700

    Reconfigured file layout, changed Harvester Giant

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-﻿// const express = require('express'); //Import the express dependency
-// const path = require('path');       // Import the path module
-// const port = 5000;                  //Save the port number where your server will be listening
-// express.json("Access-Control-Allow-Origin", "*");
-// var mime = require('mime-types');
-// var app = express();
+﻿const express = require('express'); //Import the express dependency
+const path = require('path');       // Import the path module
+const port = 5000;                  //Save the port number where your server will be listening
+express.json("Access-Control-Allow-Origin", "*");
+var mime = require('mime-types');
+var app = express();
 
-// // Use the following to handle JSON files specifically if needed
-// app.get('*/customcards.json', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'customcards.json'));
-// });
+// Use the following to handle JSON files specifically if needed
+app.get('*/customcards.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'customcards.json'));
+});
 
-// app.get('*/style.css', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/style.css'));
-// });
+app.get('*/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/style.css'));
+});
 
-// // Serve the main index.html file
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
+// Serve the main index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
 
-// app.get('regalscustomcards.dev/card/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'card.html'));
-// });
+app.get('/card/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/card.html'));
+});
 
-// app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-//   console.log(`Now listening on port ${port}`);
-// });
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`);
+});
