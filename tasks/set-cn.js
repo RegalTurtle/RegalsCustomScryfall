1a2ff9dafbc7881c80cc3e45e994391fdcf1f49c
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-10-14 18:39:08 -0400

    Finished set-cn helper

--- tasks/set-cn.js
+++ tasks/set-cn.js
@@ -1,24 +1,43 @@
-import data from "../customcards.json" assert { type: 'json' };
+import data from "../customcards.json" assert { type: "json" };
+import fs from "fs";
+
+// Make a deep copy of the original data
+let dataCopy = structuredClone(data);
 
 const set = "MTD";
 const beginCn = 85;
 const endCn = 126;
 
-data.filter((card) => {
+// Filter and get the relevant cards
+let filteredData = data.filter((card) => {
   if (card["set"] !== set) return false;
   if (card["cn"] >= beginCn && card["cn"] <= endCn) return true;
   return false;
-})
+});
 
-data.sort((a, b) => {
-  return a["name"].localeCompare(b["name"]);
-})
+// Sort the filtered data by card name
+filteredData.sort((a, b) => a["name"].localeCompare(b["name"]));
 
+// Modify the `cn` values in the sorted data
 let curCn = beginCn;
-let i = 0
-
-while (i <= data.length) {
-  data[i] = curCn;
-  i++;
+for (let i = 0; i < filteredData.length; i++) {
+  filteredData[i]["cn"] = curCn;
   curCn++;
 }
+
+// Filter `dataCopy` to remove cards in the specified range and set
+dataCopy = dataCopy.filter((card) => {
+  if (card["set"] !== set) return true;
+  if (card["cn"] >= beginCn && card["cn"] <= endCn) return false;
+  return true;
+});
+
+// Merge filtered data with the modified copy
+let mergedArray = [...filteredData, ...dataCopy];
+
+// Write the merged array back to the JSON file
+fs.writeFileSync(
+  "customcards.json",
+  JSON.stringify(mergedArray, null, 2),
+  "utf8"
+);
