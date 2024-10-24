489fcab7fea14f17953ef3983af6484a82275036
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-10-24 15:12:29 -0400

    First attempt at combining routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
 import cardRoutes from "./cards.js";
 
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const constructorMethod = (app) => {
-  app.use("/card", cardRoutes);
+  app.use(express.static(path.join(__dirname, "client")));
+
+  app.use("/cardjson", cardRoutes);
   // similar to switch, always does top to bottom
+  app.use("*/customcards.json", async (req, res) => {
+    res.sendFile(path.resolve(__dirname, "customcards.json"));
+  });
+
+  app.use("*/style.css", async (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/style.css"));
+  });
+
+  // Serve the main index.html file
+  app.use("/", async (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/index.html"));
+  });
+
+  app.use("/card/*", async (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/card.html"));
+  });
+
+  app.get("/search/*", async (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/search.html"));
+  });
   app.use("*", (req, res) => {
     return res.status(404).json({ error: "Not found" });
   });
