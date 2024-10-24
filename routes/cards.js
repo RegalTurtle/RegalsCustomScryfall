7316ea566ae6344e1bc1b02cc11f446986cc5826
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-10-24 15:05:22 -0400

    Added root /card/ path

--- routes/cards.js
+++ routes/cards.js
@@ -3,6 +3,15 @@ const router = Router();
 import cardData from "../data/cards.js";
 import validation from "../data/validation.js";
 
+router.route("/").get(async (req, res) => {
+  try {
+    const card = await cardData.getAllCards();
+    return res.json(card);
+  } catch (e) {
+    return res.status(404).json({ error: e.message });
+  }
+});
+
 router.route("/:set/:cn").get(async (req, res) => {
   let { set, cn } = req.params;
   try {
