57ef2f45279a3a0e4c631344283534fd329fca10
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-10-24 15:13:49 -0400

    Duct tape fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename).replace("/routes", "");
 
 const constructorMethod = (app) => {
   app.use(express.static(path.join(__dirname, "client")));
