30fa42e9954b0db6fdb7434d08f5965c0b92e161
RegalTurtle24 <thysvanderschoot@gmail.com>
2024-11-20 20:13:48 -0500

    Making errors more clear

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,7 @@ const constructorMethod = (app) => {
   });
 
   app.use("*", (req, res) => {
-    return res.status(404).json({ error: "Route not found" });
+    return res.status(404).json({ error: "Route not found", route: req.route });
   });
 };
 
